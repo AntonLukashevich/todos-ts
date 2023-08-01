5957a9fee6c26d3a097f26d250fb1c116c07dc6c
Aliona Kovalenko <kovalenkoalena789@gmail.com>
2023-08-01 20:17:54 +0300

    Added request pane

---
+++ src/components/RequestPane/RequestPane.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { StyledRequestPane } from "./styled";
+
+const RequestPane = () => {
+  return <StyledRequestPane>Request</StyledRequestPane>;
+};
+
+export default RequestPane;
---
+++ src/components/RequestPane/styled.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const StyledRequestPane = styled.div`
+  font-size: 20px;
+  background-color: yellow;
+  width:45%;
+`
