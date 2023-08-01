88733d8e1d13da00ce8055d13c442144673e646e
Aliona Kovalenko <kovalenkoalena789@gmail.com>
2023-08-01 20:16:25 +0300

    Added sidebar

---
+++ src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { StyledSidebar } from "./styled";
+
+const Sidebar = () => {
+    return <StyledSidebar>Sidebar</StyledSidebar>;
+};
+
+export default Sidebar;
---
+++ src/components/Sidebar/styled.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const StyledSidebar = styled.div`
+  font-size: 16px;
+  background-color: black;
+  color:#fff;
+  width:10%;
+`
