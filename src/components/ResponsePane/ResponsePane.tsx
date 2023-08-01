6b07335948a5dd0b55a537d1b19f290adb01b075
Aliona Kovalenko <kovalenkoalena789@gmail.com>
2023-08-01 20:17:35 +0300

    Added response pane

---
+++ src/components/ResponsePane/ResponsePane.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { StyledResponsePane } from "./styled";
+
+const ResponsePane = () => {
+  return <StyledResponsePane>Response</StyledResponsePane>;
+};
+
+export default ResponsePane;
---
+++ src/components/ResponsePane/styled.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const StyledResponsePane = styled.div`
+  font-size: 20px;
+  background-color: green;
+  width:45%;
+`
